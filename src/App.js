b724863afa2d652495c17c242af13ecd9b09a849
VukashinO <vukashin@hotmail.com>
2020-06-06 17:41:24 +0200

    added structure icons and some material ui css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './Theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Footer from './Footer';
+import Home from './Home';
 
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/services' component={() => <div>services</div>} />
+					<Route exact path='/customsoftware' component={() => <div>customsoftware</div>} />
+					<Route exact path='/mobileapps' component={() => <div>mobileapps</div>} />
+					<Route exact path='/websites' component={() => <div>websites</div>} />
+					<Route exact path='/revolution' component={() => <div>revolution</div>} />
+					<Route exact path='/about' component={() => <div>about</div>} />
+					<Route exact path='/contact' component={() => <div>contact</div>} />
+					<Route exact path='/estimate' component={() => <div>estimate</div>} />
+				</Switch>
+				<Footer />
+			</BrowserRouter>
+		</ThemeProvider>
+	);
+};
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+
+const Home = () => {
+    return (
+        <Grid container spacing={10}>
+            <Grid item xs={12} sm={6}>
+                1
+            </Grid>
+            <Grid item xs={12} sm={6}>
+               2
+            </Grid>
+        </Grid>
+
+    )
+}
+
+export default Home
